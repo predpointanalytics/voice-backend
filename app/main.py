9db341f1a7fe75cab65dc416b571a92bc99cb52f
bhuvanesh <iambhuvanesh@gmail.com>
2023-04-13 15:04:49 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -194,7 +194,7 @@ async def get_post(request: Request):
     signal, fs =torchaudio.load('temp22.wav', channels_first=False)
     signal = a_norm(signal, fs)
     emb =  classifier.encode_batch(signal)
-    data = numpy.array(emb[0])
+    data = numpy.array(emb[0][0])
 
     # buffer = io.BytesIO()
     # torch.save(emb[0], buffer)
